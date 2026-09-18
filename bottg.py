fb74cae3265007af3f78ae47a8ad8bb8ff4386a4
Yndsik <94436920+Yndsik@users.noreply.github.com>
2026-09-19 02:58:51 +0500

    Update bottg.py

--- bottg.py
+++ bottg.py
@@ -6,6 +6,30 @@ import threading
 import time
 import urllib.parse
 import urllib.request
+import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+# Фейковый сервер для прохождения проверки портов Render
+class HealthCheckHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+    def log_message(self, format, *args):
+        pass
+
+
+def run_health_check_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
+    server.serve_forever()
+
+
+threading.Thread(target=run_health_check_server, daemon=True).start()
 
 TOKEN = "8932170200:AAHpxbAuLChcEkQqaIofxOBUfyN8eVyEvAM"
 API_URL = f"https://api.telegram.org/bot{TOKEN}/"
